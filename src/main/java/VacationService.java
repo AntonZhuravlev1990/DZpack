1c38d860ed30f75eb15cc4956c73aa7dc2e541cc
Neto study <antonzhuravlev831@gmail.com>
2025-07-20 19:12:21 +0800

    first commit

---
+++ src/main/java/VacationService.java
@@ -0,0 +1,21 @@
+
+
+public class VacationService {
+
+    public int calcVacation(int income, int expenses, int threshold) {
+        int balance = 0;
+        int vacations = 0;
+        for (int month = 0; month < 12; month++) {
+            if (balance >= threshold) {
+                balance -= expenses;
+                balance = balance / 3;
+                vacations++;
+            } else {
+                balance += income - expenses;
+            }
+        }
+        return vacations;
+
+    }
+}
+
---
+++ src/test/java/VacationServiceTest.java
@@ -0,0 +1,23 @@
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class VacationServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(nullValues = {
+            "10_000,3_000,20_000"
+    })
+    public int shouldCalculateThreeVacationMonths(int income, int expenses, int threshold, int expected) {
+        VacationService service = new VacationService();
+        int result = service.calcVacation(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, result);
+        return expected;
+    }
+ 
+
+
+}
+
