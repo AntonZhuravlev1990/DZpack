3c831f3e56bea42f98febc82673e419b7ef4180a
Neto study <antonzhuravlev831@gmail.com>
2025-07-21 18:04:45 +0800

    first commit

--- src/test/java/ru/netology/service/VacationServiceTest.java
+++ src/test/java/ru/netology/service/VacationServiceTest.java
@@ -2,22 +2,21 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
     @ParameterizedTest
-    @CsvFileSource(nullValues = {
-            "10_000,3_000,20_000"
+    @CsvSource({"3, 10000, 3000, 20000"
     })
-    public int shouldCalculateThreeVacationMonths(int income, int expenses, int threshold, int expected) {
+
+    public void shouldCalculateThreeVacationMonths(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
+
         int result = service.calcVacation(income, expenses, threshold);
 
         Assertions.assertEquals(expected, result);
-        return expected;
-    }
- 
 
+    }
 
 }
 
